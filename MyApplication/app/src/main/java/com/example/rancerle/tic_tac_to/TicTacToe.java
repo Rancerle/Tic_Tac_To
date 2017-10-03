dbf3e2dca512e86c87d3b4d80f82efa19423410a
Reilly Q. Curran <reillycurran880@gmail.com>
2017-10-02 20:14:09 -0400

    completely implemented class TicTacToe

--- MyApplication/app/src/main/java/com/example/rancerle/tic_tac_to/TicTacToe.java
+++ MyApplication/app/src/main/java/com/example/rancerle/tic_tac_to/TicTacToe.java
@@ -6,6 +6,109 @@ package com.example.rancerle.tic_tac_to;
 
 public class TicTacToe
 {
+    public static final int SIDE = 3;
+    private int turn;
+    private int [][] game;
+
+    public TicTacToe()
+    {
+        game = new int[SIDE][SIDE];
+        resetGame();
+    }
+
+    public int play( int row, int col )
+    {
+        int currentTurn = turn;
+        if ( row >= 0 && col >= 0 && row < SIDE && col < SIDE && game[row][col] == 0 )
+        {
+            game[row][col] = turn;
+
+            if ( turn == 1 )
+            {
+                turn = 2;
+            }
+
+            else
+            {
+                turn = 1;
+            }
+
+            return currentTurn
+        }
+
+        else
+        {
+            return 0;
+        }
+    }
+
+    public int whoWon()
+    {
+        int rows = checkRows();
+
+        if (rows > 0)
+        {
+            return rows;
+        }
+
+        int cols = checkColumns();
+
+        if ( cols > 0 )
+        {
+            return cols;
+        }
+
+        int diagonals = checkDiagonals();
+
+        if( diagonals > 0 )
+        {
+            return diagonals;
+        }
+
+        return 0;
+    }
+
+    public int checkRows()
+    {
+        for ( int row = 0; row < SIDE; row++ )
+        {
+            if( game[row][0] != 0 && game[row][0] == game[row][1] && game[row][1] == game[row][2] )
+            {
+                return game[row][0];
+            }
+        }
+
+        return 0;
+    }
+
+    public int checkColumns()
+    {
+        for ( int col = 0; col < SIDE; col++ )
+        {
+            if( game[0][col] != 0 && game[0][col] == game[1][col] && game[1][col] == game[2][col] )
+            {
+                return game[0][col];
+            }
+        }
+
+        return 0;
+    }
+
+    public int checkDiagonals()
+    {
+        if ( game[0][0] != 0 && game[0][0] == game[1][1] && game[1][1] == game[2][2])
+        {
+            return game[0][0];
+        }
+
+        if(game[0][2] != 0 && game[0][2] == game[1][1] && game[1][1] == game[2][0])
+        {
+            return game[0][2];
+        }
+
+        return 0;
+    }
+
     public boolean canNotPlay()
     {
         boolean result = true;
