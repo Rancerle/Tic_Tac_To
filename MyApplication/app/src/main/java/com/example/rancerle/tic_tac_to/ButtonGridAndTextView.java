5699242315ffa3059743437182fa269d07016d95
Reilly Q. Curran <reillycurran880@gmail.com>
2017-10-02 23:53:52 -0400

    Implemented icomplete Version 5

---
+++ MyApplication/app/src/main/java/com/example/rancerle/tic_tac_to/ButtonGridAndTextView.java
@@ -0,0 +1,97 @@
+package com.example.rancerle.tic_tac_to;
+
+/**
+ * Created by Rancerle on 10/2/2017.
+ */
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.TextView;
+
+public class ButtonGridAndTextView extends GridLayout
+{
+    private int side;
+    private Button [][] buttons;
+    private TextView status;
+
+    public ButtonGridAndTextView(Context context, int width, int newSide, View.OnClickListener listener)
+    {
+        super( context );
+        side = newSide;
+
+        setColumnCount( side );
+        setRowCount( side + 1);
+
+        buttons = new Button[side][side];
+        for( int row = 0; row < side; row++ )
+        {
+            for( int col = 0; col < side; col++ )
+            {
+                buttons[row][col] = new Button( context );
+                buttons[row][col].setTextSize( ( int ) ( width * .2 ) );
+                buttons[row][col].setOnClickListener( listener );
+                addView( buttons[row][col], width, width );
+            }
+        }
+
+        status = new TextView( context );
+        GridLayout.Spec rowSpec = GridLayout.spec(side, 1);
+        GridLayout.Spec colSpec = GridLayout.spec(0, side);
+        GridLayout.LayoutParams lpStatus = new GridLayout.LayoutParams( rowSpec, colSpec );
+        status.setLayoutParams( lpStatus );
+
+        status.setWidth( side * width );
+        status.setHeight( width );
+        status.setGravity( Gravity.CENTER );
+        status.setBackgroundColor( Color.GREEN );
+        status.setTextSize( ( int ) ( width / .15 ) );
+
+        addView( status );
+    }
+
+    public void setStatusText( String text )
+    {
+        status.setText( text );
+    }
+
+    public void setStatusBackgroundColor( int color )
+    {
+        status.setBackgroundColor( color );
+    }
+
+    public void setButtonText( int row, int col, String text )
+    {
+        buttons[row][col].setText( text );
+    }
+
+    public boolean isButton( Button b, int row, int col )
+    {
+        return ( b == buttons[row][col] );
+    }
+
+    public void resetButtons( )
+    {
+        for( int row = 0; row < side; row++ )
+        {
+            for( int col = 0; col < side; col = 0 )
+            {
+                buttons[row][col].setText( "" );
+            }
+        }
+    }
+
+    public void enableButtons( boolean enabled )
+    {
+        for( int row = 0; row < side; row++ )
+        {
+            for( int col = 0; col < side; col++ )
+            {
+                buttons[row][col].setEnabled( enabled );
+            }
+        }
+    }
+}
--- MyApplication/app/src/main/java/com/example/rancerle/tic_tac_to/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/rancerle/tic_tac_to/MainActivity.java
@@ -1,117 +1,31 @@
 package com.example.rancerle.tic_tac_to;
 
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.graphics.Point;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.GridLayout;
-import android.util.Log;
-import android.graphics.Color;
-import android.view.Gravity;
-import android.widget.TextView;
-import android.support.v7.app.AlertDialog;
-
 
 public class MainActivity extends AppCompatActivity
 {
     private TicTacToe tttGame;
-    private Button [][] buttons;
-    private TextView status;
+    private ButtonGridAndTextView tttView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         tttGame = new TicTacToe( );
-        buildGuibyCode( );
-    }
-
-    public void buildGuibyCode( )
-    {
         Point size = new Point( );
-        getWindowManager( ) . getDefaultDisplay( ). getSize( size );
+        getWindowManager( ).getDefaultDisplay( ).getSize( size );
         int w = size.x / TicTacToe.SIDE;
-
-        GridLayout gridLayout = new GridLayout( this );
-        gridLayout.setColumnCount( TicTacToe.SIDE );
-        gridLayout.setRowCount( TicTacToe.SIDE + 1 );
-
-        buttons = new Button[TicTacToe.SIDE][TicTacToe.SIDE];
-        ButtonHandler bh = new ButtonHandler( );
-        for ( int row = 0; row < TicTacToe.SIDE; row++ )
-        {
-            for( int col = 0; col < TicTacToe.SIDE; col++ )
-            {
-                buttons[row][col] = new Button( this );
-                buttons[row][col].setTextSize( ( int ) ( w * .2 ) );
-                buttons[row][col].setOnClickListener( bh );
-                gridLayout.addView( buttons[row][col], w, w);
-            }
-        }
-
-        status = new TextView( this );
-        GridLayout.Spec rowSpec = GridLayout.spec( TicTacToe.SIDE, 1 );
-        GridLayout.Spec columnSpec = GridLayout.spec( 0, TicTacToe.SIDE );
-        GridLayout.LayoutParams lpStatus = new GridLayout.LayoutParams( rowSpec, columnSpec );
-        status.setLayoutParams( lpStatus );
-
-        status.setWidth( TicTacToe.SIDE * w );
-        status.setHeight( w );
-        status.setGravity( Gravity.CENTER );
-        status.setBackgroundColor( Color.GREEN );
-        status.setTextSize( ( int ) ( w * .15 ) );
-        status.setText( tttGame.result() );
-
-        gridLayout.addView( status );
-
-        setContentView( gridLayout );
-    }
-
-    public void update( int row, int col )
-    {
-        int play = tttGame.play( row, col );
-
-        if ( play == 1 )
-        {
-            buttons[row][col].setText( "X" );
-        }
-
-        else if ( play == 2 )
-        {
-            buttons[row][col].setText( "O" );
-        }
-
-        if( tttGame.isGameOver() )
-        {
-            status.setBackgroundColor( Color.RED );
-            enableButtons( false );
-            status.setText( tttGame.result() );
-            showNewGameDialog( );
-        }
-    }
-
-    public void enableButtons ( boolean enabled )
-    {
-        for( int row = 0; row < TicTacToe.SIDE; row++ )
-        {
-            for( int col = 0; col < TicTacToe.SIDE; col++ )
-            {
-                buttons[row][col].setEnabled( enabled );
-            }
-        }
-    }
-
-    public void resetButtons( )
-    {
-        for( int row = 0; row < TicTacToe.SIDE; row++ )
-        {
-            for( int col = 0; col < TicTacToe.SIDE; col++ )
-            {
-                buttons[row][col].setText( "" );
-            }
-        }
+        ButtonHandler bh = new ButtonHandler();
+        tttView = new ButtonGridAndTextView( this, w, TicTacToe.SIDE, bh );
+        tttView.setStatusText( tttGame.result() );
+        setContentView( tttView );
     }
 
     public void showNewGameDialog( )
@@ -129,14 +43,28 @@ public class MainActivity extends AppCompatActivity
     {
         public void onClick( View v )
         {
-            Log.w( "MainActivity", "Inside onClick, v = " + v);
             for ( int row = 0; row < TicTacToe.SIDE; row++ )
             {
                 for ( int col = 0; col < TicTacToe.SIDE; col++ )
                 {
-                    if ( v == buttons[row][col] )
+                    if ( tttView.isButton( ( Button ) v, row, col ) )
                     {
-                        update( row, col );
+                        int play = tttGame.play( row, col );
+                        if ( play == 1 )
+                        {
+                            tttView.setButtonText( row, col, "X" );
+                        }
+                        else if ( play == 2 )
+                        {
+                            tttView.setButtonText( row, col, "O" );
+                        }
+                        if( tttGame.isGameOver() )
+                        {
+                            tttView.setBackgroundColor( Color.RED );
+                            tttView.enableButtons( false );
+                            tttView.setStatusText( tttGame.result() );
+                            showNewGameDialog();
+                        }
                     }
                 }
             }
@@ -150,10 +78,10 @@ public class MainActivity extends AppCompatActivity
             if( id == -1 )
             {
                 tttGame.resetGame();
-                enableButtons( true );
-                resetButtons();
-                status.setBackgroundColor( Color.GREEN );
-                status.setText( tttGame.result() );
+                tttView.enableButtons( true );
+                tttView.resetButtons( );
+                tttView.setStatusBackgroundColor( Color.GREEN );
+                tttView.setStatusText( tttGame.result() );
             }
 
             else if( id == -2 )
